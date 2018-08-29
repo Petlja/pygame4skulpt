1130a0d2f42b9a4071ae80fab64979a27554c9f4
Radenko Pavlovic <radenkopavlovic994@gmail.com>
2018-08-29 16:04:47 +0200

    fix

--- pygamelib/pygamelib.js
+++ pygamelib/pygamelib.js
@@ -224,6 +224,109 @@ var PygameLib = {};
         return mod;
     };
 
+    function time_Clock($gbl, $loc) {
+        $loc.__init__ = new Sk.builtin.func(function (self) {
+            Sk.abstr.sattr(self, 'prevTime', Sk.builtin.none.none$, false);
+            Sk.abstr.sattr(self, 'getTime', Sk.builtin.none.none$, false);
+            Sk.abstr.sattr(self, 'rawTime', Sk.ffi.remapToPy(0), false);
+            Sk.abstr.sattr(self, 'fpsArray', Sk.ffi.remapToPy([]), false);
+            Sk.abstr.sattr(self, 'fpsIdx', Sk.ffi.remapToPy(0));
+            return Sk.builtin.none.none$;
+        }, $gbl);
+        $loc.__init__.co_name = new Sk.builtins['str']('__init__');
+
+        $loc.tick = new Sk.builtin.func(function (self, framerate) {
+            var currTime = Date.now();
+            var mills = 0;
+            if (Sk.ffi.remapToJs(Sk.abstr.gattr(self, 'prevTime', false)) !== null) {
+                var prevTime = Sk.ffi.remapToJs(Sk.abstr.gattr(self, 'prevTime', false));
+                mills = (currTime - prevTime);
+            }
+            Sk.abstr.sattr(self, 'prevTime', Sk.ffi.remapToPy(currTime), false);
+            Sk.abstr.sattr(self, 'getTime', Sk.ffi.remapToPy(mills), false);
+            var arr = Sk.ffi.remapToJs(Sk.abstr.gattr(self, 'fpsArray', false));
+            var idx = Sk.ffi.remapToJs(Sk.abstr.gattr(self, 'fpsIdx', false));
+            if (arr.length < 10) {
+                arr.push(mills);
+            } else {
+                arr[idx] = mills;
+            }
+            idx = (idx + 1) % 10;
+            Sk.abstr.sattr(self, 'fpsArray', Sk.ffi.remapToPy(arr), false);
+            Sk.abstr.sattr(self, 'fpsIdx', Sk.ffi.remapToPy(idx), false);
+            if (framerate !== undefined) {
+                var timeout = 1000 / Sk.ffi.remapToJs(framerate);
+                return new Sk.misceval.promiseToSuspension(
+                    new Promise(function(resolve) {
+                    var f = function() {
+                        Sk.abstr.sattr(self, 'rawTime', Sk.ffi.remapToPy(Date.now() - currTime), false);
+                        while (currTime + timeout >= Date.now()) {}
+                        resolve(mills);
+                    };
+                    Sk.setTimeout(f, 1);
+                }));
+            }
+            Sk.abstr.sattr(self, 'rawTime', Sk.ffi.remapToPy(Date.now() - currTime), false);
+            return Sk.ffi.remapToPy(mills);
+        }, $gbl);
+        $loc.tick.co_name = new Sk.builtins['str']('tick');
+        $loc.tick.co_varnames = ['framerate'];
+        $loc.tick.$defaults = [Sk.ffi.remapToPy(0)];
+
+        $loc.tick_busy_loop = new Sk.builtin.func(function (self, framerate) {
+            // TODO: Should this method have a different implementation?
+            var currTime = Date.now();
+            var mills = 0;
+            if (Sk.ffi.remapToJs(Sk.abstr.gattr(self, 'prevTime', false)) !== null) {
+                var prevTime = Sk.ffi.remapToJs(Sk.abstr.gattr(self, 'prevTime', false));
+                mills = (currTime - prevTime);
+            }
+            Sk.abstr.sattr(self, 'prevTime', Sk.ffi.remapToPy(currTime), false);
+            Sk.abstr.sattr(self, 'getTime', Sk.ffi.remapToPy(mills), false);
+
+            if (framerate !== undefined) {
+                var timeout = 1000 / Sk.ffi.remapToJs(framerate);
+                return new Sk.misceval.promiseToSuspension(
+                    new Promise(function(resolve) {
+                    var f = function() {
+                        Sk.abstr.sattr(self, 'rawTime', Sk.ffi.remapToPy(Date.now() - currTime), false);
+                        while (currTime + timeout >= Date.now()) {}
+                        resolve(mills);
+                    };
+                    Sk.setTimeout(f, 1);
+                }));
+            }
+            Sk.abstr.sattr(self, 'rawTime', Sk.ffi.remapToPy(Date.now() - currTime), false);
+            return Sk.ffi.remapToPy(mills);
+        }, $gbl);
+        $loc.tick_busy_loop.co_name = new Sk.builtins['str']('tick_busy_loop');
+        $loc.tick_busy_loop.co_varnames = ['framerate'];
+        $loc.tick_busy_loop.$defaults = [Sk.ffi.remapToPy(0)];
+
+        $loc.get_time = new Sk.builtin.func(function (self) {
+            return Sk.abstr.gattr(self, 'getTime', false);
+        });
+        $loc.get_time.co_name = new Sk.builtins['str']('get_time');
+
+        $loc.get_rawtime = new Sk.builtin.func(function (self) {
+            return Sk.abstr.gattr(self, 'rawTime', false);
+        });
+        $loc.get_rawtime.co_name = new Sk.builtins['str']('get_rawtime');
+
+        $loc.get_fps = new Sk.builtin.func(function(self) {
+            var arr = Sk.ffi.remapToJs(Sk.abstr.gattr(self, 'fpsArray', false));
+            if (arr.length < 10 || arr[0] === 0) {
+                return Sk.ffi.remapToPy(0);
+            }
+            var sum = 0;
+            for (var i = 0; i < 10; i++) {
+                sum += arr[i];
+            }
+            return Sk.ffi.remapToPy(sum / 10);
+        });
+    }
+    time_Clock.co_name = new Sk.builtins['str']('Clock');
+
     // pygame.font
     PygameLib.font_module = function(name) {
         mod = {};
