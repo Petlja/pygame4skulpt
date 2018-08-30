857744b1f0902300c857744a6731c376411d3847
Nebojša Vasiljević <nebojsa.vasiljevic@gmail.com>
2018-08-30 19:15:09 +0200

    fixed missing `new` when calling `Sk.builtin.func`

--- pygamelib/pygamelib.js
+++ pygamelib/pygamelib.js
@@ -1935,13 +1935,13 @@ var PygameLib = {};
     // pygame.version
     PygameLib.version_module = function(name) {
         mod = {};
-        mod.ver = Sk.builtin.func(function () {
+        mod.ver = new Sk.builtin.func(function () {
             return Sk.ffi.remapToPy("1.9.3");
         });
-        mod.vernum = Sk.builtin.func(function () {
+        mod.vernum = new Sk.builtin.func(function () {
             return Sk.builtin.tuple([1, 9, 3]);
         });
-        mod.rev = Sk.builtin.func(function () {
+        mod.rev = new Sk.builtin.func(function () {
             return Sk.builtin.none.none$;
         });
         return mod;
