38e0ecb0d49baa06bdeed1d875e10e90d6f1d66e
Lazar Milenkovic <milenkovic.lazar@gmail.com>
2018-09-13 18:46:46 +0200

    Implement image.save()

--- pygamelib/pygamelib.js
+++ pygamelib/pygamelib.js
@@ -2240,26 +2240,25 @@ var PygameLib = {};
             return Sk.ffi.remapToPy(false);
         });
         mod.save = new Sk.builtin.func(function (surf, filename) {
-
+            var image = surf.offscreen_canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+            window.location.href = image;
         });
         return mod;
-    }
+    };
 
     var load_image = function(filename) {
-        return new Sk.misceval.promiseToSuspension(new Promise(function(resolve) {
-                var img = new Image();
-                img.src = PygameLib.imgPath + Sk.ffi.remapToJs(filename);
-                img.onload = function () {
-                    var w = PygameLib.surface.width;
-                    var h = PygameLib.surface.height;
-                    var t = Sk.builtin.tuple([img.width, img.height]);
-                    var s = Sk.misceval.callsim(PygameLib.SurfaceType, t, false);
-                    var ctx = s.offscreen_canvas.getContext("2d");
-                    ctx.drawImage(img, 0, 0);
-                    resolve(s);
-                }
-            }));
-    }
+        return new Sk.misceval.promiseToSuspension(new Promise(function (resolve) {
+            var img = new Image();
+            img.src = PygameLib.imgPath + Sk.ffi.remapToJs(filename);
+            img.onload = function () {
+                var t = Sk.builtin.tuple([img.width, img.height]);
+                var s = Sk.misceval.callsim(PygameLib.SurfaceType, t, false);
+                var ctx = s.offscreen_canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0);
+                resolve(s);
+            }
+        }));
+    };
 
 }());
 
