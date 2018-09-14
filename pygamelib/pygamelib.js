940c8393362870427937f7947e0940af93400b4f
Lazar Milenkovic <milenkovic.lazar@gmail.com>
2018-09-14 15:44:22 +0200

    Custom filename when saving a surface

--- pygamelib/pygamelib.js
+++ pygamelib/pygamelib.js
@@ -2240,8 +2240,63 @@ var PygameLib = {};
             return Sk.ffi.remapToPy(false);
         });
         mod.save = new Sk.builtin.func(function (surf, filename) {
-            var image = surf.offscreen_canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-            window.location.href = image;
+            var fname = 'surface';
+            if (filename !== undefined) {
+                fname = Sk.ffi.remapToJs(filename);
+            }
+            // https://stackoverflow.com/a/34707543
+            saveAsPNG(surf.offscreen_canvas, fname);
+            function saveAsPNG(image, filename) { // No IE <11 support. Chrome URL bug for large images may crash
+                var anchorElement, event, blob;
+
+                function image2Canvas(image) {  // converts an image to canvas
+                    function createCanvas(width, height) {  // creates a canvas of width height
+                        var can = document.createElement("canvas");
+                        can.width = width;
+                        can.height = height;
+                        return can;
+                    };
+                    var newImage = canvas(img.width, img.height); // create new image
+                    newImage.ctx = newImage.getContext("2d");  // get image context
+                    newImage.ctx.drawImage(image, 0, 0); // draw the image onto the canvas
+                    return newImage;  // return the new image
+                }
+
+                if (image.toDataURL === undefined) {    // does the image have the toDataURL function
+                    image = image2Canvas(image);  // No then convert to canvas
+                }
+                // if msToBlob and msSaveBlob then use them to save. IE >= 10
+                // As suggested by Kaiido
+                if (image.msToBlob !== undefined && navigator.msSaveBlob !== undefined) {
+                    blob = image.msToBlob();
+                    navigator.msSaveBlob(blob, filename + ".png");
+                    return;
+                }
+                anchorElement = document.createElement('a');  // Create a download link
+                anchorElement.href = image.toDataURL();   // attach the image data URL
+                // check for download attribute
+                if (anchorElement.download !== undefined) {
+                    anchorElement.download = filename + ".png";  // set the download filename
+                    if (typeof MouseEvent === "function") {   // does the browser support the object MouseEvent
+                        event = new MouseEvent(   // yes create a new mouse click event
+                            "click", {
+                                view: window,
+                                bubbles: true,
+                                cancelable: true,
+                                ctrlKey: false,
+                                altKey: false,
+                                shiftKey: false,
+                                metaKey: false,
+                                button: 0,
+                                buttons: 1,
+                            }
+                        );
+                        anchorElement.dispatchEvent(event); // simulate a click on the download link.
+                    } else if (anchorElement.fireEvent) {    // if no MouseEvent object try fireEvent
+                        anchorElement.fireEvent("onclick");
+                    }
+                }
+            }
         });
         return mod;
     };
